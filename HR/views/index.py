3c069705b16b9833ad5ef279cf12fc93f5ab3e7e
robbyhecht <robbyhecht@gmail.com>
2019-01-28 17:51:11 -0600

    added index.html, index.py in views updated urls, added a placeholder navbar.

---
+++ HR/views/index.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+
+def index(request):
+  return render(request, 'HR/index.html')
+
+  # def index(request):
+  # # return HttpResponse("Welcome to the music database.")
+  # print("REQUEST", request)
+  # artist_list = Artist.objects.order_by('name')
+  # context = {'artist_list': artist_list}
+  # return render(request, 'history/index.html', context)
+
+  
