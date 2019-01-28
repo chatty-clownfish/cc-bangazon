54cac3ec8495c24e773c02616c9bda30da29f457
John Wood <johnnathanwood@gmail.com>
2019-01-28 09:57:10 -0600

    moved .git down a folder into bangazon

---
+++ HR/models/trainingModels.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.urls import reverse
+
+class Artist(models.Model):
+  name = models.CharField(default="", max_length=100)
+  start_date = models.CharField(default="", max_length=100)
+  end_date = models.CharField(default="", max_length=100)
+  maxAttendees = models.CharField(default="", max_length=100)
+
+  def __str__(self):
+    return self.name
