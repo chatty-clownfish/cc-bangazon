aaac2428b3db8154731ba47775c80b2d20d1a7f8
John Wood <johnnathanwood@gmail.com>
2019-01-28 11:57:43 -0600

    made it a req to enter start and end date for training

--- HR/models/trainingModels.py
+++ HR/models/trainingModels.py
@@ -3,9 +3,9 @@ from django.urls import reverse
 
 class Training(models.Model):
   name = models.CharField(default="", max_length=100)
-  start_date = models.DateField(default="", max_length=100)
-  end_date = models.DateField(default="", max_length=100)
-  maxAttendees = models.IntegerField(default="", max_length=100)
+  start_date = models.DateField()
+  end_date = models.DateField()
+  maxAttendees = models.IntegerField()
   employees = models.ManyToManyField(Employee, through='EmployeeTraining')
 
   def __str__(self):
