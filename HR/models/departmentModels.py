a346160bdc5fcaa5a52c0c8d0c4989530dce45f9
Andrew Herring <andrewoherring@gmail.com>
2019-01-28 10:18:19 -0600

    department model and blank readme

---
+++ HR/models/departmentModels.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Department(models.Model):
+  name = models.CharField(default="", max_length=100)
+  budget = models.IntegerField(default="", max_length=50)
+
+  def __str__(self):
+    return self.name
