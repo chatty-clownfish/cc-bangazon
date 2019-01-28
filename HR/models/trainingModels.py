49cea82a2043fc8b2a4ff8d4ad896a605a5e044c
John Wood <johnnathanwood@gmail.com>
2019-01-28 11:03:58 -0600

    fixed training model

--- HR/models/trainingModels.py
+++ HR/models/trainingModels.py
@@ -3,9 +3,9 @@ from django.urls import reverse
 
 class Training(models.Model):
   name = models.CharField(default="", max_length=100)
-  start_date = models.CharField(default="", max_length=100)
-  end_date = models.CharField(default="", max_length=100)
-  maxAttendees = models.CharField(default="", max_length=100)
+  start_date = models.DateField(default="", max_length=100)
+  end_date = models.DateField(default="", max_length=100)
+  maxAttendees = models.IntegerField(default="", max_length=100)
   employees = models.ManyToManyField(Employee, through='EmployeeTraining')
 
   def __str__(self):
