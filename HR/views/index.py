db79b8937d9360f725198c5a18ef32986968f28a
robbyhecht <robbyhecht@gmail.com>
2019-01-29 11:05:17 -0600

    made index page display header

--- HR/views/index.py
+++ HR/views/index.py
@@ -5,18 +5,4 @@ from django.http import HttpResponse, HttpResponseRedirect
 def index(request):
   # return HttpResponse("Hello, world. You're at the Death Star index.")
   print("REQUEST", request)
-  return render(request, 'history/index.html')
-
-
-
-# def index(request):
-#   return render(request, 'HR/index.html')
-
-  # def index(request):
-  # # return HttpResponse("Welcome to the music database.")
-  # print("REQUEST", request)
-  # artist_list = Artist.objects.order_by('name')
-  # context = {'artist_list': artist_list}
-  # return render(request, 'history/index.html', context)
-
-  
+  return render(request, 'HR/index.html')
