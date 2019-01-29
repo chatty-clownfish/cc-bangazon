e166ab90e9db33bee6100393f25c7e7342a95f24
robbyhecht <robbyhecht@gmail.com>
2019-01-29 09:50:54 -0600

    getting closer with home page

--- HR/urls.py
+++ HR/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
 from . import views
 
+
 app_name = 'HR'
 urlpatterns = [
   # ex: /HR/
   path('', views.index, name='index'),
 ]
 
-
-
-
-
-
-
-# views.IndexView.as_view() instead of views.index?
--- HR/views/index.py
+++ HR/views/index.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 
+
 def index(request):
-  return render(request, 'HR/index.html')
+  # return HttpResponse("Hello, world. You're at the Death Star index.")
+  print("REQUEST", request)
+  return render(request, 'history/index.html')
+
+
+
+# def index(request):
+#   return render(request, 'HR/index.html')
 
   # def index(request):
   # # return HttpResponse("Welcome to the music database.")
