3bf3f6040d08ac643269bccd62ab1d76278361cd
Daniel Combs <danielcombs@Daniels-MacBook-Pro.local>
2019-01-29 11:11:32 -0600

    commit to merge

--- HR/models/trainingModels.py
+++ HR/models/trainingModels.py
@@ -1,14 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
-
-
 class Training(models.Model):
   name = models.CharField(default="", max_length=100)
   start_date = models.DateField()
   end_date = models.DateField()
   maxAttendees = models.IntegerField()
-  employees = models.ManyToManyField("Employee", through='EmployeeTraining')
+  employees = models.ManyToManyField(Employee, through='EmployeeTraining')
 
   def __str__(self):
     return self.name
--- HR/views/employeeViews.py
+++ HR/views/employeeViews.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def employeeList(request, employee_id):
+    Employee_list = Employee.objects.filter(employe_id = employee_id)
+    print(Employee_list)
+    context = {'Employee_list' : Employee_list}
+    return render(request, 'HR/views/employeeList.html', context)
