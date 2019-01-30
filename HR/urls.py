21bf797243ee919c95932502ca291c5ae0d78b37
John Wood <johnnathanwood@gmail.com>
2019-01-30 11:40:57 -0600

    GET and POST method working for ticket #9

--- HR/urls.py
+++ HR/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
   # department index
   path('departments/', views.departmentIndex, name='departments'),
   # department details
-  path('departments/<int:dept_id>/', views.details, name='deptDetails')
+  path('departments/<int:dept_id>/', views.details, name='deptDetails'),
 
   path('employees/', views.employeeList , name = 'employees'),
   
   path('training/', views.trainingList, name='trainings'),
-  path('addTraining/', views.addTraining, name='add'),
+  path('addTraining/', views.add_training_program, name='add'),
 
 ]
 
--- HR/views/trainingViews.py
+++ HR/views/trainingViews.py
@@ -4,20 +4,20 @@ from HR.models import Training
 
 def trainingList(request):
     training_list = Training.objects.all()
-    context = { 'training_list' : training_list }
+    context = {'training_list': training_list}
     return render(request, 'HR/training/training.html', context)
 
-def addTraining(request):
-    context = { 'hello' : 'hello' }
-    return render(request, 'HR/training/addTraining.html', context)
 
 def add_training_program(request):
-  name = request.POST['training_name']
-  start_date= request.POST['training_startDate']
-  end_date = request.POST['training_endDate']
-  maxEnrollment = request.POST['training_maxEnrollment']
-  t = Training(name = name, startDate = start_date, endDate = end_date, maxEnrollment = maxEnrollment)
-  t.save()
-  response = redirect('./training')
-  return response
-
+    if request.method == "POST":
+        name = request.POST['training_name']
+        start_date= request.POST['training_startDate']
+        end_date = request.POST['training_endDate']
+        maxAttendees = request.POST['training_maxEnrollment']
+        t = Training(name = name, start_date = start_date, end_date = end_date, maxAttendees = maxAttendees)
+        t.save()
+        response = redirect('HR:trainings')
+        return response
+    if request.method == "GET":
+        return render(request, 'HR/training/addTraining.html')
+    
