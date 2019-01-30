a5f552b69f2e462d8bbff105036b336ed2bf756e
robbyhecht <robbyhecht@gmail.com>
2019-01-30 11:18:19 -0600

    adding employees is fully functional

--- HR/views/employeeViews.py
+++ HR/views/employeeViews.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from HR.models.departmentModels import Department
 from HR.models.employeeModels import Employee
 from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
 
@@ -12,7 +13,6 @@ def addEmployee(request):
 
   if request.method == "GET":
   #render the form page
-    print("hello")
 
     department_list = Department.objects.all()
     print(department_list)
@@ -21,19 +21,19 @@ def addEmployee(request):
     return render(request, 'HR/employee/addemployee.html', context)
 
   if request.method == "POST":
-    first_name = request.POST["first_name"]
+    # first_name = request.POST["first_name"]
     last_name = request.POST["last_name"]
     start_date = request.POST["start_date"]
     is_supervisor = request.POST["is_supervisor"]
     department_id = get_object_or_404(Department, pk=request.POST["department_id"])
+    # print(first_name)
+    employee_list = Employee.objects.create(first_name=request.POST["first_name"], last_name=last_name, start_date=start_date, is_supervisor=is_supervisor, department_id=department_id.id)
+    # reverse statement is telling urls.py which address to go to and which method to invoke
+    return HttpResponseRedirect(reverse('HR:employees'))
 
-    employee_list = Employee.objects.create(first_name=first_name, last_name=last_name, start_date=start_date, is_supervisor=is_supervisor, department_id=department_id.id)
 
-    department_list = Department.objects.all()
 
-    context = {"employee_list" : employee_list, "department_list" : department_list}
 
-    return render(request, 'HR/employee/employees.html', context)
 
 
 # Create your views here.
@@ -42,5 +42,5 @@ def employeeList(request):
     Employee_list = Employee.objects.all()
     print(Employee_list)
     context ={'Employee_list' : Employee_list, 'Department_list': Department_list}
-    return render(request, 'HR/employee/employee.html', context)
+    return render(request, 'HR/employee/Employee.html', context)
 
