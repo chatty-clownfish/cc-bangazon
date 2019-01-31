0dd449281d17fce43054fb83b887faf1998d7744
Andrew Herring <andrewoherring@gmail.com>
2019-01-31 15:48:46 -0600

    doc strings

--- HR/models/departmentModels.py
+++ HR/models/departmentModels.py
@@ -1,7 +1,11 @@
 from django.db import models
 
+# model used to construct Departments
+
 class Department(models.Model):
+  # departments have two properties, name and budget
   name = models.CharField(default="", max_length=100)
+  # budget must be a number
   budget = models.IntegerField()
 
   def __str__(self):
--- bangazon/urls.py
+++ bangazon/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # include the app urls
     path('HR/', include('HR.urls')),
 ]
 
