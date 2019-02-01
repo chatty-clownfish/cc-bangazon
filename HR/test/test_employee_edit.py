525980d62b33a8e8fc46f518367646acc6aad376
robbyhecht <robbyhecht@gmail.com>
2019-02-01 14:40:42 -0600

    3 tests working for employee edit -- 200, assert equal, 302

--- HR/test/test_employee_edit.py
+++ HR/test/test_employee_edit.py
@@ -21,11 +21,9 @@ class EmployeeEditTest(TestCase):
       is_supervisor= 1,
       department_id= new_dept.id,
     )
-    print("EMPLOYEE", new_employee)
 
     # test whether the get is directing to the correct url
     response = self.client.get(reverse('HR:editemployee', args=(new_employee.id,)))
-    print("EDITRESPONSE", response)
     self.assertEqual(response.status_code, 200)
 
 
@@ -44,13 +42,11 @@ class EmployeeEditTest(TestCase):
       is_supervisor= 0,
       department_id= dept_of_stuff.id,
     )
-    print("EMPLOYEE", new_employee)
 
     # test whether the get is retrieving template with employee info as existing value
 
     response = self.client.get(reverse('HR:editemployee', args=(new_employee.id,)))
     form_return = 'First Name: <input type="text" name="first_name" value="Emmet" required=true><br />\n  Last Name: <input type="text" name="last_name" value="Ray" required=true><br />\n  Start date: <input type="date" name="start_date" value="2008-05-03" required=true><br />\n  Supervisor Status:\n  <input type="radio" name="is_supervisor" value=1>Yes\n  <input type="radio" name="is_supervisor" checked="checked" value=0>No<br />\n  Department: <select name="department_id" id="employee_department">\n    \n    <option value="1">Department of Stuff</option>\n    \n  </select><br /><br />\n  <input type="submit" value="Save Employee">\n</form>\n'.encode()
-    print('FORMRETURN', form_return)
     self.assertIn(form_return, response.content)
 
     # test that edited info is posting from the form back to the employee detail
@@ -70,6 +66,8 @@ class EmployeeEditTest(TestCase):
       is_supervisor= 1,
       department_id= dept_of_things.id,
     )
-    print("EMPLOYEE", new_employee)
 
-  
+    response = self.client.post(reverse('HR:editemployee', args=(new_employee.id, )), {'first_name': 'Bruce', 'last_name': 'Willis', 'start_date': '2010-05-05', 'is_supervisor': 1, "department_id": dept_of_things.id})
+    # Getting 302 back because we have a success url and the view is redirecting
+    self.assertEqual(response.status_code, 302)
+
