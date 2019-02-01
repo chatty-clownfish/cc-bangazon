2c6b8cbecbebc4ec0a6e5c8173c4bd5f33390803
robbyhecht <robbyhecht@gmail.com>
2019-01-31 23:22:07 -0600

    corrected employeeadd post test for new form format... added response code 200 test for employeeedit get test

---
+++ HR/test/test_employee_edit.py
@@ -0,0 +1,29 @@
+import unittest
+from django.test import TestCase
+from django.urls import reverse
+from HR.models.employeeModels import Employee
+from HR.models.departmentModels import Department
+
+class EmployeeEditTest(TestCase):
+
+  def test_get_employee_edit_form(self):
+
+    # create instances of department and employee
+
+    new_dept = Department.objects.create(
+      name="Department of Stuff",
+      budget="8000"
+    )
+    new_employee = Employee.objects.create(
+      first_name="Django",
+      last_name="Reinhardt",
+      start_date="2007-12-04",
+      is_supervisor= 1,
+      department_id= new_dept.id,
+    )
+    print("EMPLOYEE", new_employee)
+
+    # test whether the get is directing to the correct url
+    response = self.client.get(reverse('HR:editemployee', args=(new_employee.id,)))
+    print("EDITRESPONSE", response)
+    self.assertEqual(response.status_code, 200)
