4a0ec6ab638baa54c963c779b4e7529ce7da40cf
robbyhecht <robbyhecht@gmail.com>
2019-02-01 13:36:20 -0600

    set up instance for post test

--- HR/test/test_employee_edit.py
+++ HR/test/test_employee_edit.py
@@ -46,12 +46,30 @@ class EmployeeEditTest(TestCase):
     )
     print("EMPLOYEE", new_employee)
 
-    # test whether the get is retrieving template
+    # test whether the get is retrieving template with employee info as existing value
 
     response = self.client.get(reverse('HR:editemployee', args=(new_employee.id,)))
     form_return = 'First Name: <input type="text" name="first_name" value="Emmet"><br />\n  Last Name: <input type="text" name="last_name" value="Ray"><br />\n  Start date: <input type="date" name="start_date" value="2008-05-03"><br />\n  Supervisor Status:\n  <input type="radio" name="is_supervisor" value=1>Yes\n  <input type="radio" name="is_supervisor" checked="checked" value=0>No<br />\n  Department: <select name="department_id" id="employee_department">\n    \n    <option value="1">Department of Stuff</option>\n    \n  </select><br /><br />\n  <input type="submit" value="Save Employee">\n</form>\n'.encode()
     print('FORMRETURN', form_return)
     self.assertIn(form_return, response.content)
 
+    # test that edited info is posting from the form back to the employee detail
+
+  def test_post_edit_employee(self):
+
+    # create instance of employee
+
+    dept_of_things = Department.objects.create(
+      name="Department of Things",
+      budget="10000"
+    )
+    new_employee = Employee.objects.create(
+      first_name="Bruce",
+      last_name="Willis",
+      start_date="2010-05-05",
+      is_supervisor= 1,
+      department_id= dept_of_things.id,
+    )
+    print("EMPLOYEE", new_employee)
 
   
